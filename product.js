5298f758726a2dbf69702089f5da42901067459a
endoun <endounshop@gmail.com>
2021-11-29 15:46:24 +0700

    pl

--- product.js
+++ product.js
@@ -1,24 +1,23 @@
-var product =
-[
-    {
+var product ={
+    sh0001: {
         brand: 'nike',
         sku: 'shni0001',
         name: 'Jordan 1 Milian',
         price: 745000
     },
-    {
+    sh0002: {
         brand: 'adidas',
         sku: 'shad0001',
         name: 'Yeezy 350 Xám',
         price: 485000
     },
-    {
+    sh0003: {
         brand: 'other',
         sku: 'shot0001',
         name: 'MLB Gót Xanh Bóng',
         price: 345000
     }
-];
+};
 
     // var pri = ShowPrice(pro.name);
 function show(pro) {
@@ -28,7 +27,7 @@ function show(pro) {
                 <img class="show__box--img" src="../img/shoes/${pro.brand}/${pro.sku}-1.jpg" alt="${pro.name}" onerror="this.src=&#39;img/img-share.png;"/> 
                 <div class="show__box--brand">${pro.brand.toUpperCase()}</div> 
                 <div class="show__box--name">${pro.name}</div> 
-                <div class="show__box--sale-off">&nbsp;&nbsp;</div> 
+                <div class="show__box--sale-off">&nbsp;${pro.price}&nbsp;</div> 
                 <div class="show__box--price"> 
                     <strong class="show__price--cost">&nbsp;&nbsp;</strong> 
                     <strong class="show__price--saled">&nbsp;${pro.price}đ&nbsp;</strong> 
@@ -37,4 +36,6 @@ function show(pro) {
         </div>
     `
 }
-document.getElementById('showAll').innerHTML = product.map(show).join('');
+for (let ii = 0; ii < product.lenght; ++ii) {
+document.getElementById('showAll').innerHTML = product.sh000`${ii}`.name;
+}
